c62df4365437550083cfdd1b12029e5ba8780f0f
Krendelev <krendelev@gmail.com>
2018-09-07 03:12:01 +0300

    implement main feature

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Program to count the occurrences of each word in a given text.
 
-# Project Goals
+## Getting Started
+
+Program takes plain text file and returns list of 10 words with the most occurrences in that text along with their counts.
+
+```bash
+$ python lang_frequency.py some_text.txt
+[('the', 3734), ('and', 2977), ('a', 1794), ('to', 1708), ('of', 1455), ('he', 1173), ('was', 1162), ('it', 1016), ('in', 935), ('that', 860)]
+```
+
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,25 @@
+import sys
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as file_handler:
+        return file_handler.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = (w.strip('.,:;"?!-()_').lower() for w in text.split() if w not in '-–')
+    return Counter(words).most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        text_from_file = load_data(sys.argv[1])
+    except IndexError:
+        print('Please specify path to text file')
+    except FileNotFoundError:
+        print('File not found')
+    except ValueError:
+        print('Not a valid text file')
+    else:
+        print(get_most_frequent_words(text_from_file))
